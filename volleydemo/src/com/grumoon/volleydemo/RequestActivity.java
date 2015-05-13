46b99b617a4f095fe963c32c8d8d51e93b4dea6e
wuhuiheu <wuhuiheu@gmail.com>
2015-05-13 19:56:11 +0800

    添加工程文件

---
+++ volleydemo/src/com/grumoon/volleydemo/RequestActivity.java
@@ -0,0 +1,93 @@
+package com.grumoon.volleydemo;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+
+import com.example.volleydemo.R;
+import com.grumoon.volleydemo.fragment.ImageLoaderFragment;
+import com.grumoon.volleydemo.fragment.ImageRequestFragment;
+import com.grumoon.volleydemo.fragment.JsonRequestFragment;
+import com.grumoon.volleydemo.fragment.NetworkImageViewFragment;
+import com.grumoon.volleydemo.fragment.PostRequestFragment;
+import com.grumoon.volleydemo.fragment.StringRequestFragment;
+import com.grumoon.volleydemo.fragment.XmlRequestFragment;
+import com.grumoon.volleydemo.util.Constants;
+
+public class RequestActivity extends Activity{
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		int frIndex = getIntent().getIntExtra(Constants.Extra.FRAGMENT_INDEX, 0);
+		Fragment fr;
+		String tag;
+		int titleRes;
+		switch (frIndex) {
+		default:
+		case StringRequestFragment.INDEX:
+			tag = StringRequestFragment.class.getSimpleName();
+			
+			fr = getFragmentManager().findFragmentByTag(tag);
+			if (fr == null) {
+				fr = new StringRequestFragment();
+			}
+			titleRes = R.string.string_request;
+			break;
+		case JsonRequestFragment.INDEX:
+			tag = JsonRequestFragment.class.getSimpleName();
+			fr = getFragmentManager().findFragmentByTag(tag);
+			if (fr == null) {
+				fr = new JsonRequestFragment();
+			}
+			titleRes = R.string.json_request;
+			break;
+		case ImageRequestFragment.INDEX:
+			tag = ImageRequestFragment.class.getSimpleName();
+			fr = getFragmentManager().findFragmentByTag(tag);
+			if (fr == null) {
+				fr = new ImageRequestFragment();
+				fr.setArguments(getIntent().getExtras());
+			}
+			titleRes = R.string.image_request;
+			break;
+		case ImageLoaderFragment.INDEX:
+			tag = ImageLoaderFragment.class.getSimpleName();
+			fr = getFragmentManager().findFragmentByTag(tag);
+			if (fr == null) {
+				fr = new ImageLoaderFragment();
+			}
+			titleRes = R.string.image_loader;
+			break;
+		case NetworkImageViewFragment.INDEX:
+			tag = NetworkImageViewFragment.class.getSimpleName();
+			fr = getFragmentManager().findFragmentByTag(tag);
+			if (fr == null) {
+				fr = new NetworkImageViewFragment();
+			}
+			titleRes = R.string.network_image_view;
+			break;
+		case XmlRequestFragment.INDEX:
+			tag = XmlRequestFragment.class.getSimpleName();
+			fr = getFragmentManager().findFragmentByTag(tag);
+			if (fr == null) {
+				fr = new XmlRequestFragment();
+			}
+			titleRes = R.string.xml_request;
+			break;
+		case PostRequestFragment.INDEX:
+			tag = PostRequestFragment.class.getSimpleName();
+			fr = getFragmentManager().findFragmentByTag(tag);
+			if (fr == null) {
+				fr = new PostRequestFragment();
+			}
+			titleRes = R.string.post_request;
+			break;
+
+		}
+
+		setTitle(titleRes);
+		getFragmentManager().beginTransaction().replace(android.R.id.content, fr, tag).commit();
+	}
+}
